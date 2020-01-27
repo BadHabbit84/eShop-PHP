50895d7b94a12c25fe4ecb84b1703aa955af23a1
Attila <attila@p4d.co.uk>
2020-01-27 13:35:50 +0000

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,7 @@
+<?php
+include_once 'includes/header.php';
+include_once 'includes/footer.php';
+include_once 'templates/frontpage.php';
+include_once 'lib/User.php';
+include_once 'lib/Item.php';
+include_once 'lib/DB.php';
